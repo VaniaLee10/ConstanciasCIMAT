3c796863b1b7a25462e0ce6d945f035a480c53c7
VaniaLee10 <vaniafanybff@gmail.com>
2023-08-17 23:05:27 -0600

    Modal envio contancias

--- script/modal_visualizacion.js
+++ script/modal_visualizacion.js
@@ -205,3 +205,39 @@ function modalconstancias() {
     };
 
 }
+
+//---------------------------------Modal 4
+function modal_envio_constancias() {
+    // Get the modal
+    var modal = document.getElementById("modal_envio_constancias");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("envio_constancias");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[2];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    //When the user clicks on Cerrar / Cancelar
+    var btn_cerrar = btn = document.getElementById("cerrar_modal_envio");
+    btn_cerrar.onclick = function () {
+        modal.style.display = "none";
+    }
+
+}
